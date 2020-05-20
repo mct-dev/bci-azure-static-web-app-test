2e25ba7eb7fb5233ceb6e2d0532ae6f5460810d1
Michael Tobias <axis139@gmail.com>
2020-05-19 18:10:33 -0700

    adds input styling and post request

--- app/src/App.js
+++ app/src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 
 function App() {
   const [data, setData] = React.useState('')
+  const [name, setName] = React.useState('')
+
   const fetchIt = async () => {
-    const result = await fetch('/api/products')
+    const stateName = name
+    const result = await fetch('/api/products', { method: 'POST', data: { name: stateName }})
     const data = await result.text()
     console.log(data)
     setData(data)
   }
 
+  const inputStyle = {
+    fontSize: '18px',
+    padding: '5px',
+    marginBottom: '20px'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <input style={inputStyle} type="text" placeholder="Enter a name..." onChange={(e) => setName(e.target.value)} />
         <button style={{ padding: '10px', fontSize: '20px', cursor: 'pointer' }} onClick={fetchIt}>Get Api Response From Azure Function</button>
         <p>
           API response:
